0ae78f7f909413be7004948a309d3bdc78c726ff
akshay-goel-g <akshay.goel.g@gmail.com>
2019-09-04 03:11:11 +0000

    Java file added

--- Try.java
+++ Try.java
@@ -2,6 +2,6 @@ public class Try{
 public static void main(String a[])
 {
 for(int i=0;i<10;i++)
-System.out.println("Hi this is the iterational number  "+i);
+System.out.println("Hi this is the new iterational number  "+i);
 }
 }
